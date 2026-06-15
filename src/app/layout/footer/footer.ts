982fc70f5f10235ef48907eca61e85e48ac0ec14
EduardoPereyra <eduardo.pereyrayraola@gmail.com>
2026-06-15 17:53:42 +0200

    Better footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   currentYear: number = new Date().getFullYear();
+
+  faPhone = faPhone;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
+  faGithub = faGithub;
 }
